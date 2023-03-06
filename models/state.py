48c8d8a73f6df45e2cc1aa67d983728a721172a6
vigobin <vgobin@outlook.com>
2023-03-06 16:30:57 -0600

    Task 6: Added cities relationship to State

--- models/state.py
+++ models/state.py
@@ -2,9 +2,13 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+
+    cities = relationship('City', cascade='all, delete',
+                          back_populates='state')
