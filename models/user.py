d6b5763e107743b6148bd57dd44987b79358d033
oladele90 <oladelemiro07@gmail.com>
2023-03-07 19:24:27 -0800

    urp

--- models/user.py
+++ models/user.py
@@ -13,3 +13,4 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=False)
 
 places = relationship('Place', cascade="all, delete, delete-orphan", backref='user')
+reviews = relationship('Review', cascade="all, delete, delete-orphan", backref='user')
