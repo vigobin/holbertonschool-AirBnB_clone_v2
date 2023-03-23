64d0754175ffc2957f0551e5895bbb98c6191826
vigobin <vgobin@outlook.com>
2023-03-23 10:15:34 -0500

    Initial commit task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Hello Flask!"""
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """route"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run()
