cf174013a7f5417d3f74f67acc1b090bb76959e0
vigobin <vgobin@outlook.com>
2023-03-24 10:01:59 -0500

    Initial commit task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Is it a number?"""
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def Hbnb():
+    """Display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Display C"""
+    return ('C {}'.format(text.replace('_', ' ')))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """display python"""
+    return ('Python {}'.format(text.replace('_', ' ')))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    return ('{} is a number'.format(n))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
