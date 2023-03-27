b63b878e7c24f0065c839292c4cbe36672e0cbda
vigobin <vgobin@outlook.com>
2023-03-27 10:26:47 -0500

    Initial commit task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,3 +68,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         self.__session = scoped_session(sessionmaker(
             bind=self.__engine, expire_on_commit=False))
+
+    def close(self):
+        """close session"""
+        self.__session.close()
