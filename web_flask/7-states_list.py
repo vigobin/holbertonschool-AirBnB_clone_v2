95e66f745dd18e73c6b852a88142757b6a7a9bd4
vigobin <vgobin@outlook.com>
2023-03-27 14:49:34 -0500

    Added files task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""List of states"""
+from flask import Flask, request, render_template
+from markupsafe import escape
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """List the states"""
+    states = storage.all('State')
+
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(sql):
+    """Remove current SQL Alchemy session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
